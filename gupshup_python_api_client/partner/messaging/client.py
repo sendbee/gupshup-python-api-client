199ead3009f3971d8f60749f291dc4f8390b9fc3
Ivan Arar <ivan.arar@gmail.com>
2021-07-07 15:26:25 +0200

    feature/create-template

--- gupshup_python_api_client/partner/messaging/client.py
+++ gupshup_python_api_client/partner/messaging/client.py
@@ -1,7 +1,8 @@
 from gupshup_python_api_client import constants
 from gupshup_python_api_client.bind import bind_request
 from gupshup_python_api_client.partner.messaging import query_params
-from gupshup_python_api_client.partner.messaging.models import TemplateList
+from gupshup_python_api_client.partner.messaging.models import TemplateList, \
+    Template
 
 
 class Messaging:
@@ -15,4 +16,14 @@ class Messaging:
         model=TemplateList,
         query_parameters=query_params.ListTemplates,
         description='Get WhatsApp message template list'
+    )
+
+    create_template = bind_request(
+        method=constants.RequestConst.POST,
+        api_path='/partner/app/<app_id>/templates',
+        header={'Connection': 'keep-alive'},
+        force_single_model_response=True,
+        model=Template,
+        query_parameters=query_params.CreateTemplate,
+        description='Create WhatsApp message template'
     )
--- gupshup_python_api_client/partner/messaging/query_params.py
+++ gupshup_python_api_client/partner/messaging/query_params.py
@@ -5,3 +5,15 @@ class ListTemplates(QueryParams):
     """Parameters for fetching a list of WhatsApp message templates"""
 
     app_id = 'app_id', 'Gupshup App ID'
+
+
+class CreateTemplate(QueryParams):
+    """Parameters for creating a WhatsApp message template"""
+
+    element_name = 'elementName', 'Gupshup App ID'
+    language_code = 'languageCode', 'Gupshup App ID'
+    category = 'category', 'Gupshup App ID'
+    template_type = 'templateType', 'Gupshup App ID'
+    vertical = 'vertical', 'Gupshup App ID'
+    content = 'content', 'Gupshup App ID'
+    buttons = 'buttons', 'Gupshup App ID'
